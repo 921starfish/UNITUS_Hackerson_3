cf9b4046425c13ce8171fb81f190b44f487e7c64
921starfish <yuya.921.hoshino@gmail.com>
2015-03-07 23:11:55 +0900

    ボタンおいてみた

--- app/src/main/java/com/example/q21starfish/unitus_hackerson_3/MainActivity.java
+++ app/src/main/java/com/example/q21starfish/unitus_hackerson_3/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changeLabel(View view){
+        TextView tv = (TextView)findViewById(R.id.myTextView);
+        tv.setText("OSK");
+
     }
 }
