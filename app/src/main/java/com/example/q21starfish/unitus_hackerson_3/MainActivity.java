9531214b87655d75bf8b9920b71949a579e68791
921starfish <yuya.921.hoshino@gmail.com>
2015-03-08 12:03:33 +0900

    コメントアウトの解除

--- app/src/main/java/com/example/q21starfish/unitus_hackerson_3/MainActivity.java
+++ app/src/main/java/com/example/q21starfish/unitus_hackerson_3/MainActivity.java
@@ -43,9 +43,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /*public void changeLabel(View view){
+    public void changeLabel(View view){
         TextView tv = (TextView)findViewById(R.id.myTextView);
         tv.setText("OSK");
-
-    }*/
+    }
 }
